409ed578983e030483089ad415124897f1f7c88b
Nasim Khan <wk4933942@gmail.com>
2026-07-19 20:40:37 +0530

    Add files via upload

---
+++ janta.py
@@ -0,0 +1,30 @@
+import streamlit as st
+st.title("ABOUT OUR SCHOOL")
+st.header("shree janta secondary school mayadevi-5,baluhawa")
+st.subheader("ESTD : 2045")
+st.text("our principle:RAM NIWAS CHAUHAN")
+st.text("TOTAL TEACHER:20")
+st.text("TOTAL STUDENT:400")
+st.header("MAIN MOTIVE OUR SCHOOL")
+st.write("-Our mission is to provide quality education and help students achieve their dreams.")
+st.header("Core Values (The principles that guide our school)")
+st.write("""
+- Respect
+- Honesty
+- Discipline
+- Responsibility
+- Teamwork
+- Creativity
+         """)
+st.header("faclities:")
+st.write("""
+*2 Computer lab 
+*Science lab
+*Electric lab
+         """)
+
+st.header("Departments")
+st.write("""
+*Technical
+*General
+    """)
