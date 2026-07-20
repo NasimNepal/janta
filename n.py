45ed8054224a940356a4c9ebfb87bbd576d818dd
Nasim Khan <wk4933942@gmail.com>
2026-07-20 12:58:12 +0530

    Add files via upload

---
+++ n.py
@@ -0,0 +1,55 @@
+import streamlit as st
+
+st.image("school2.jpg", caption="School Image")
+
+st.title("ABOUT OUR SCHOOL")
+st.header("Shree Janta Secondary School Mayadevi-5,Baluhawa")
+st.subheader("ESTD : 2045")
+st.text("our principle:RAM NIWAS CHAUHAN")
+st.text("TOTAL TEACHER:20")
+st.text("TOTAL STUDENT:400")
+st.header("MAIN MOTIVE OUR SCHOOL")
+st.write("-Our mission is to provide quality education and help students achieve their dreams.")
+st.header("Core Values (The principles that guide our school)")
+st.write("""
+- Respect
+- Honesty
+- Discipline
+- Responsibility
+- Teamwork
+- Creativity
+         """)
+st.header("Faclities:")
+st.write("""
+*2 Computer lab 
+*Science lab
+*Electric lab
+         """)
+
+st.text("teacher section:")
+st.write("""
+-all rounder:Ramesh sir & Niraj sir
+-Math teacher:Manoj sir
+-Science teacher:manoj vishowakarma
+-English teacher:dharamvir chauhan
+-nepali teacher:Hira ghimire
+
+        """)
+
+st.header("Departments")
+st.write("""
+*Technical
+*General
+    """)
+
+st.text("If you intrested in school ")
+
+st.write("Email:wk3943@gmail.com")
+st.write("Phone:9807446464")
+
+st.caption("Design by the Nasim")
+
+st.text("OJT(ON JOB TRAINING)")
+
+st.video("allu.mp4")
+
